f0711ab2762d590077358236b2afdcb09fda726e
jhugman <jhugman@jhugman-10147.local>
2014-03-19 23:33:01 +0000

    Change logtag.

--- src/org/mozilla/android/synthapk/C.java
+++ src/org/mozilla/android/synthapk/C.java
@@ -7,7 +7,7 @@ public final class C {
         // no instantiation
     }
 
-    public static final String TAG = "synthapklib";
+    public static final String TAG = "Gecko-synthapklib";
     public static final String WEBAPP_MIMETYPE = "application/webapp";
     public static final String FENNEC_PACKAGE_NAME = "org.mozilla.firefox";
 
